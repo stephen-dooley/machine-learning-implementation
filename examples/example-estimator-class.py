e194ec0c0cba491b3adf96e0c617d09b2e5ca16b
stephen-dooley <s.dooley6@nuigalway.ie>
2015-11-17 14:12:46 +0000

    create iPython notebook to solve logisitc regression problem

---
+++ examples/example-estimator-class.py
@@ -0,0 +1,11 @@
+class Estimator(object):
+
+    def fit(self, X, y=None):
+        """Fits estimator to data. """
+        # set state of ``self``
+        return self
+
+    def predict(self, X):
+        """Predict response of ``X``. """
+        # compute predictions ``pred``
+        return pred
